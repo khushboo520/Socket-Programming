4ba677d961a4ef6043884fd6ea9ad1652d40d67b
khushboo520 <55673856+khushboo520@users.noreply.github.com>
2019-09-22 20:12:30 -0500

    Add files via upload

---
+++ Replica Management and Consistency/ClientApplication/Client.Designer.cs
@@ -0,0 +1,163 @@
+﻿//Khushboo Babariya
+//1001668208
+namespace ClientWindowsFormsApplication
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtRegisterClient = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRegisterClient
+            // 
+            this.txtRegisterClient.Location = new System.Drawing.Point(210, 35);
+            this.txtRegisterClient.Name = "txtRegisterClient";
+            this.txtRegisterClient.Size = new System.Drawing.Size(251, 38);
+            this.txtRegisterClient.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34,35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Register";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 32);
+            this.label4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1199, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 57);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Disconnect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(40, 85);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(0, 32);
+            this.labelProgress.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1115, 38);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1199, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(263, 61);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // answer
+            // 
+            this.answer.Location = new System.Drawing.Point(40, 223);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(500, 38);
+            this.answer.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 57);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Register_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1501, 1021);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRegisterClient);
+            this.Name = "Client";
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtRegisterClient;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Replica Management and Consistency/ClientApplication/Client.cs
@@ -0,0 +1,161 @@
+﻿//Khushboo Babariya
+//1001668208
+using System;
+using System.Data;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace ClientWindowsFormsApplication
+{
+
+    delegate void SetTextCallback(string text);
+    public partial class Client : Form
+    {
+        TcpClient clientSocket = new TcpClient();
+        double localCopy = 1;
+        StringBuilder expression = new StringBuilder();
+        public Client()
+        {
+            InitializeComponent();
+            //Connect to server
+            clientSocket.Connect("127.0.0.1", 8888);
+            labelProgress.Text = "Server Connected.";
+        }
+
+        /// <summary>
+        /// Register client to server and request for random time wait once client registered successfully
+        /// Source:http://csharp.net-informations.com/communications/csharp-multi-threaded-client-socket.htm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Register_Click(object sender, EventArgs e)
+        {
+            if (!clientSocket.Connected)
+            {
+                clientSocket = new TcpClient();
+                clientSocket.Connect("127.0.0.1", 8888);
+            }
+
+            if (!string.IsNullOrEmpty(txtRegisterClient.Text))
+            {
+                errorProvider1.Clear();
+                NetworkStream serverStream = clientSocket.GetStream();
+                //Send registeration message in http format
+                string registerRequest = HttpMessageFormat.HttpMessageBody.Replace("{ClientName}", txtRegisterClient.Text);
+                string contnentLength = HttpMessageFormat.HttpMessageContentLength.Replace("{Length}", registerRequest.Length.ToString());
+                byte[] requestStream = System.Text.Encoding.ASCII.GetBytes(HttpMessageFormat.HttpMessagRequesteLine.Replace("{Method}", "POST")
+                    + HttpMessageFormat.HttpMessageHost + HttpMessageFormat.HttpMessageUserAgent + HttpMessageFormat.HttpMessageContentType + contnentLength + registerRequest);
+                serverStream.Write(requestStream, 0, requestStream.Length);
+                serverStream.Flush();
+                //Receive reply from server,parse the same and display
+                byte[] responseStream = new byte[(int)clientSocket.ReceiveBufferSize];
+                int bytesRead = serverStream.Read(responseStream, 0, responseStream.Length);
+                string response = System.Text.Encoding.ASCII.GetString(responseStream);
+                label4.Text = Environment.NewLine + "Response: " + Environment.NewLine + response;
+                txtRegisterClient.Enabled = false;
+                //Start thread continuously check for poll request from server
+                Thread waitRequestProcessor = new Thread(PollRequestProcessor);
+                waitRequestProcessor.Start();
+            }
+            else
+            {
+                errorProvider1.SetError(txtRegisterClient, "Please enter Client Name.");
+            }
+        }
+
+        /// <summary>
+        /// Disconnect client socket based on user request
+        /// </summary>
+        ///  <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            clientSocket.Close();
+        }
+
+
+        /// <summary> Calculate expression entered by user on local copy</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //calculate expression in airthmatic order and if it results to NAN then clear expression input
+            object newValue = new DataTable().Compute(localCopy.ToString() + textBox1.Text, null).ToString();
+            if (!newValue.Equals("∞"))
+            {
+                localCopy = double.Parse(newValue.ToString());
+                expression.Append(textBox1.Text);
+                label4.Text += Environment.NewLine + expression.ToString();
+                answer.Text = newValue.ToString();
+            }
+            else
+            {
+                textBox1.Text = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Server polls the request.
+        /// </summary>
+        private void PollRequestProcessor()
+        {
+            Thread.Sleep(2000);
+            try
+            {
+                while (true)
+                {
+                    if (clientSocket.Connected)
+                    {
+                        NetworkStream serverStream = clientSocket.GetStream();
+                        byte[] message = new byte[clientSocket.ReceiveBufferSize];
+                        serverStream.Read(message, 0, message.Length);
+                        string clientData = System.Text.Encoding.ASCII.GetString(message);
+                        string[] message1 = clientData.Split('\n');
+                        JObject json = JObject.Parse(message1.Last());
+                        //Send expression to server when receive poll request
+                        if (json["ResponseMessage"].ToString() == "Poll")
+                        {
+                            string requestJsonMessage = @"{'ClientName':'" + txtRegisterClient.Text + "','Expression':'" + expression.ToString() + "'}";
+                            string waitContnentLength = HttpMessageFormat.HttpMessageContentLength.Replace("{Length}", requestJsonMessage.Length.ToString());
+                            string requestMessage = HttpMessageFormat.HttpMessagRequesteLine.Replace("{Method}", "POST")
+                                + HttpMessageFormat.HttpMessageHost + HttpMessageFormat.HttpMessageUserAgent + HttpMessageFormat.HttpMessageContentType + waitContnentLength + requestJsonMessage.ToString();
+                            byte[] waitStream = System.Text.Encoding.ASCII.GetBytes(requestMessage);
+                            NetworkStream serverStream1 = clientSocket.GetStream();
+                            serverStream1.Write(waitStream, 0, waitStream.Length);
+
+                        }
+                        else
+                        {
+                            //Receive answer from server and replace local copy with the same
+                            localCopy = double.Parse(json["ResponseMessage"].ToString());
+                            if (this.label4.InvokeRequired)
+                            {
+                                SetTextCallback d = new SetTextCallback(SetText);
+                                this.Invoke(d, new object[] { Environment.NewLine + "Value after poll: " + localCopy.ToString() });
+                            }
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception y)
+            {
+                clientSocket.Close();
+            }
+        }
+
+        /// <summary>
+        /// Sets the text to the label for displaying current status of server
+        /// </summary>
+        /// <param name="text">The text.</param>
+        private void SetText(string text)
+        {
+            this.label4.Text = this.label4.Text + text + Environment.NewLine;
+        }
+    }
+}
